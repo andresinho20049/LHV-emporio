afe729b08e6985dd2766309a48d775b34f587024
andresinho20049 <andre.andresinho2009@hotmail.com>
2024-02-09 00:09:10 -0300

    feat: Report PDF with react-pdf/renderer

--- next.config.js
+++ next.config.js
@@ -2,9 +2,17 @@ const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: "standalone",
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, os: false, path: false };
+
+    return config;
+  },
+  experimental: {
+    serverComponentsExternalPackages: ["puppeteer-core"],
+  },
 };
 
 module.exports = withContentlayer(nextConfig);
